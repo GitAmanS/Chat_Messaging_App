0e262f55d56c54bdfef69cc52422615bc482bdf4
Aman Shaikh <amanshaikh8624@gmail.com>
2023-12-28 04:27:14 -0800

    chat backend

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,7 +1,6 @@
 // routes/chat.js
 const express = require('express');
 const { Op } = require('sequelize');
-const { authenticateSocket } = require('../middleware/authMiddleware');
 const ChatMessage = require('../models/ChatMessage');
 
 const router = express.Router();
